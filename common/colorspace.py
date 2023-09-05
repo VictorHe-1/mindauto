6838e92bc4e0cbef54dd0e8f487960f6da7313ee
VictorHe <zhiyuhe@u.nus.edu>
2023-09-05 17:26:45 +0800

    Add colorspace conversion

---
+++ common/colorspace.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+def convert_color_factory(src, dst):
+    code = getattr(cv2, f'COLOR_{src.upper()}2{dst.upper()}')
+
+    def convert_color(img):
+        out_img = cv2.cvtColor(img, code)
+        return out_img
+
+    convert_color.__doc__ = f"""Convert a {src.upper()} image to {dst.upper()}
+        image.
+
+    Args:
+        img (ndarray or str): The input image.
+
+    Returns:
+        ndarray: The converted {dst.upper()} image.
+    """
+
+    return convert_color
+
+
+bgr2hsv = convert_color_factory('bgr', 'hsv')
+
+hsv2bgr = convert_color_factory('hsv', 'bgr')
