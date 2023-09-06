03600d311551e2c9a9138c1e0d33c0e1f0c3f069
VictorHe <zhiyuhe@u.nus.edu>
2023-09-06 16:30:18 +0800

    Add ConfigDict

--- common/__init__.py
+++ common/__init__.py
@@ -5,11 +5,12 @@ from .common_utils import (load_from_serialized, dump,
                            imnormalize, impad_to_multiple, is_list_of, mkdir_or_exist, imwrite,
                            imshow, imread)
 from .colorspace import bgr2hsv, hsv2bgr
+from .config import ConfigDict
 
 __all__ = [
     'load_from_serialized', 'dump', 'list_from_file',
     'rescale_size', 'imresize', 'imflip', 'impad',
     'imtranslate', 'imshear', 'imrotate', 'imrescale', 'imfrombytes',
     'check_file_exist', 'bgr2hsv', 'hsv2bgr', 'imnormalize', 'impad_to_multiple',
-    'is_list_of', 'mkdir_or_exist', 'imwrite', 'imshow', 'imread'
+    'is_list_of', 'mkdir_or_exist', 'imwrite', 'imshow', 'imread', 'ConfigDict'
 ]
