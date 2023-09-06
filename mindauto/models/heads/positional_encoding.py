9423b8f4573f851eca79cb44bbc7a3513cd5b010
VictorHe <zhiyuhe@u.nus.edu>
2023-09-06 14:50:28 +0800

    Add positional encoding

---
+++ mindauto/models/heads/positional_encoding.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+from mindspore import nn, ops
+
+
+class LearnedPositionalEncoding(nn.Cell):
+    """Position embedding with learnable embedding weights.
+
+    Args:
+        num_feats (int): The feature dimension for each position
+            along x-axis or y-axis. The final returned dimension for
+            each position is 2 times of this value.
+        row_num_embed (int, optional): The dictionary size of row embeddings.
+            Default 50.
+        col_num_embed (int, optional): The dictionary size of col embeddings.
+            Default 50.
+        init_cfg (dict or list[dict], optional): Initialization config dict.
+    """
+
+    def __init__(self,
+                 num_feats,
+                 row_num_embed=50,
+                 col_num_embed=50):
+        super(LearnedPositionalEncoding, self).__init__()
+        self.row_embed = nn.Embedding(row_num_embed, num_feats)
+        self.col_embed = nn.Embedding(col_num_embed, num_feats)
+        self.num_feats = num_feats
+        self.row_num_embed = row_num_embed
+        self.col_num_embed = col_num_embed
+
+    def construct(self, mask):
+        """Forward function for `LearnedPositionalEncoding`.
+
+        Args:
+            mask (Tensor): ByteTensor mask. Non-zero values representing
+                ignored positions, while zero values means valid positions
+                for this image. Shape [bs, h, w].
+
+        Returns:
+            pos (Tensor): Returned position embedding with shape
+                [bs, num_feats*2, h, w].
+        """
+        h, w = mask.shape[-2:]
+        x = ops.arange(w)
+        y = ops.arange(h)
+        x_embed = self.col_embed(x)
+        y_embed = self.row_embed(y)
+        pos = ops.cat(
+            (x_embed.unsqueeze(0).tile((h, 1, 1)), y_embed.unsqueeze(1).tile(
+                (1, w, 1))),
+            axis=-1).permute(2, 0,
+                             1).unsqueeze(0).tile((mask.shape[0], 1, 1, 1))
+        return pos
+
+    def __repr__(self):
+        """str: a string that describes the module"""
+        repr_str = self.__class__.__name__
+        repr_str += f'(num_feats={self.num_feats}, '
+        repr_str += f'row_num_embed={self.row_num_embed}, '
+        repr_str += f'col_num_embed={self.col_num_embed})'
+        return repr_str
