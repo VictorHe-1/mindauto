4c46e03bc0ffcaba9dc0f580ae8b0075980b0242
yuedongli1 <yuedongli1@huawei.com>
2023-09-07 20:19:26 +0800

    add iou_loss, l1_loss

---
+++ mindauto/losses/smooth_l1_loss.py
@@ -0,0 +1,79 @@
+from mindspore import nn, ops
+
+from mindauto.losses.utils import weight_reduce_loss
+
+class L1Loss(nn.Cell):
+    """L1 loss.
+
+    Args:
+        reduction (str, optional): The method to reduce the loss.
+            Options are "none", "mean" and "sum".
+        loss_weight (float, optional): The weight of loss.
+    """
+
+    def __init__(self,
+                 reduction: str = 'mean',
+                 loss_weight: float = 1.0) -> None:
+        super().__init__()
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def construct(self,
+                pred,
+                target,
+                weight = None,
+                avg_factor = None,
+                reduction_override = None):
+        """Forward function.
+
+        Args:
+            pred (Tensor): The prediction.
+            target (Tensor): The learning target of the prediction.
+            weight (Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Defaults to None.
+
+        Returns:
+            Tensor: Calculated loss
+        """
+        if weight is not None and not ops.any(weight > 0):
+            if pred.ndim == weight.ndim + 1:
+                weight = weight.unsqueeze(1)
+            return (pred * weight).sum()
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        loss_bbox = self.loss_weight * l1_loss(
+            pred, target)
+        loss_bbox = weight_reduce_loss(loss_bbox, weight, reduction=reduction, avg_factor=avg_factor)
+        return loss_bbox
+
+
+def l1_loss(pred, target):
+    """L1 loss.
+
+    Args:
+        pred (Tensor): The prediction.
+        target (Tensor): The learning target of the prediction.
+
+    Returns:
+        Tensor: Calculated loss
+    """
+    if target.numel() == 0:
+        return pred.sum() * 0
+
+    assert pred.shape == target.shape
+    loss = ops.abs(pred - target)
+    return loss
+
+
+if __name__ == '__main__':
+    pred = ops.rand((6, 6, 6))
+    target = ops.rand((6, 6, 6))
+    loss_instance = L1Loss()
+    loss = loss_instance(pred, target)
+    print(loss)
