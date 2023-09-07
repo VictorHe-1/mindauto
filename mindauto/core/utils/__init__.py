c29790d2eb9985196bdaacafb78c93eca57fe000
VictorHe <zhiyuhe@u.nus.edu>
2023-09-07 09:59:54 +0800

    Add NiceRepr in core.utils

--- mindauto/core/utils/__init__.py
+++ mindauto/core/utils/__init__.py
@@ -1,4 +1,5 @@
 from .misc import multi_apply
 from .util_random import ensure_rng
+from .util_mixins import NiceRepr
 
-__all__ = ['multi_apply', 'ensure_rng']
+__all__ = ['multi_apply', 'ensure_rng', 'NiceRepr']
