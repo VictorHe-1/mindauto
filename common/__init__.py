c627387653869ac52bef0b10ea8fc1fbdd02cb8d
VictorHe <zhiyuhe@u.nus.edu>
2023-09-11 16:13:38 +0800

    Add color_eval

--- common/__init__.py
+++ common/__init__.py
@@ -10,6 +10,7 @@ from .activation import build_activation_layer
 from .drop import build_dropout
 from .norm import build_norm_layer
 from .misc import concat_list
+from .color import color_val
 
 __all__ = [
     'load_from_serialized', 'dump', 'list_from_file',
@@ -18,5 +19,6 @@ __all__ = [
     'check_file_exist', 'bgr2rgb', 'rgb2bgr', 'bgr2hsv', 'hsv2bgr',
     'imnormalize', 'impad_to_multiple', 'bgr2hls', 'hls2bgr',
     'is_list_of', 'mkdir_or_exist', 'imwrite', 'imshow', 'imread', 'ConfigDict',
-    'build_activation_layer', 'build_dropout', 'build_norm_layer', 'concat_list'
+    'build_activation_layer', 'build_dropout', 'build_norm_layer', 'concat_list',
+    'color_val'
 ]
