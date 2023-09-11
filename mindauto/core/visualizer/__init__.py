808629737c72a95047d36d1e4aab7c62e5d02a41
VictorHe <zhiyuhe@u.nus.edu>
2023-09-11 16:16:38 +0800

    Add imshow_det_bboxes for base detector

--- mindauto/core/visualizer/__init__.py
+++ mindauto/core/visualizer/__init__.py
@@ -1,4 +1,5 @@
 from .show_result import (show_result,
                           show_seg_result)
+from .image import imshow_det_bboxes
 
-__all__ = ['show_result', 'show_seg_result']
+__all__ = ['show_result', 'show_seg_result', 'imshow_det_bboxes']
