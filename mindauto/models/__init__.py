2357a027bf257dfc278306b8acf721add8c73c8b
VictorHe <zhiyuhe@u.nus.edu>
2023-09-11 17:37:15 +0800

    Update model builder

--- mindauto/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import _registry, builder
-from ._registry import *
-from .builder import *
-from .bevformer import *
-
-__all__ = []
-__all__.extend(builder.__all__)
-__all__.extend(_registry.__all__)
