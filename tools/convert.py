82e11583bb4c2d4229560b3d53b44b87a2ca9a1d
VictorHe <zhiyuhe@u.nus.edu>
2023-10-07 09:53:25 +0800

    Add conversion script for bevformer

---
+++ tools/convert.py
@@ -0,0 +1,25 @@
+# convert pytorch model to mindspore
+import json
+
+import torch
+
+import mindspore as ms
+
+# load param_map json
+with open("./bevformer_mapping.json", "r") as json_file:
+    param_name_map = json.load(json_file)
+
+net = torch.load("bevformer_tiny_epoch_24.pth")
+
+params_dict = net.state_dict()
+# conversion
+ms_params = []
+for name, value in params_dict.items():
+    each_param = dict()
+    if name not in param_name_map:
+        continue
+    each_param["name"] = param_name_map[name]
+    each_param["data"] = ms.Tensor(value.numpy())
+    ms_params.append(each_param)
+
+ms.save_checkpoint(ms_params, "bevformer_tiny.ckpt")
