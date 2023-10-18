0552204adaad4c199d985ce39add21355d6ae66d
VictorHe <zhiyuhe@u.nus.edu>
2023-10-18 16:53:07 +0800

    Fix lsa for graph mode

--- mindauto/core/bbox/assigners/run_lsa.py
+++ mindauto/core/bbox/assigners/run_lsa.py
@@ -1,20 +1,21 @@
-import sys
 from mindspore.scipy.ops import LinearSumAssignment
 from mindspore.scipy.utils import _mstype_check, _dtype_check
 from mindspore.common import dtype as mstype
-from mindspore import Tensor
+from mindspore import Tensor, nn
 
 
-def linear_sum_assignment(cost_matrix, maximize, dimension_limit=Tensor(sys.maxsize)):
-    func_name = 'linear_sum_assignment'
-    _mstype_check(func_name, cost_matrix, mstype.TensorType, 'cost_matrix')
-    _mstype_check(func_name, dimension_limit,
-                  mstype.TensorType, 'dimension_limit')
-    _mstype_check(func_name, maximize, mstype.TensorType, 'maximize')
-    _dtype_check(func_name, cost_matrix, [mstype.float32, mstype.float64])
-    _dtype_check(func_name, dimension_limit, [mstype.int64])
-    _dtype_check(func_name, maximize, [mstype.bool_])
+class NetLsap(nn.Cell):
+    def __init__(self):
+        super().__init__()
+        self.op = LinearSumAssignment().set_device(device_target='CPU')
 
-    solve = LinearSumAssignment()
-    solve.set_device(device_target="CPU")
-    return solve(cost_matrix, dimension_limit, maximize)
+    def construct(self, cost_matrix, maximize, dimension_limit):
+        func_name = 'linear_sum_assignment'
+        _mstype_check(func_name, cost_matrix, mstype.TensorType, 'cost_matrix')
+        _mstype_check(func_name, dimension_limit,
+                      mstype.TensorType, 'dimension_limit')
+        _mstype_check(func_name, maximize, mstype.TensorType, 'maximize')
+        _dtype_check(func_name, cost_matrix, [mstype.float32, mstype.float64])
+        _dtype_check(func_name, dimension_limit, [mstype.int64])
+        _dtype_check(func_name, maximize, [mstype.bool_])
+        return self.op(cost_matrix, dimension_limit, maximize)
