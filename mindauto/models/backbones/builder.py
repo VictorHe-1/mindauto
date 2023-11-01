f75aa60ed8bf1771f26541d5674c90957c8074d3
VictorHe <zhiyuhe@u.nus.edu>
2023-11-01 17:27:31 +0800

    Add load backbone during training

--- mindauto/models/backbones/builder.py
+++ mindauto/models/backbones/builder.py
@@ -1,3 +1,5 @@
+import mindspore as ms
+
 from .resnet import ResNet
 
 backbone_types = {'ResNet': ResNet}
@@ -7,4 +9,10 @@ def build_backbone(**kwargs):
     obj_cls = backbone_types.get(kwargs['type'])
     args = kwargs.copy()
     args.pop('type')
-    return obj_cls(**args)
+    load_path = args.pop('ckpt_load_path', None)
+    if load_path is None:
+        return obj_cls(**args)
+    else:
+        model = obj_cls(**args)
+        ms.load_checkpoint(kwargs['ckpt_load_path'], model)
+        return model
